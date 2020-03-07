c691b4b29ce3e85a1789fb80c9c28aeb0f543433
Gaël Weiler <weiler.gael@gmail.com>
2020-03-07 15:40:06 +0100

    Add the new game modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBorderAll, faUndoAlt, faRetweet } from '@fortawesome/free-solid-svg-icons'
+import Modal from './Modal'
 import '../assets/css/NavBar.scss'
 
 function NavBar() {
+  const [showNewGameModal, setShowNewGameModal] = useState(false)
+
+  /**
+   * Toggle the modal display value
+   */
+  const toggleNewGameModal = () => {
+    setShowNewGameModal(!showNewGameModal)
+  }
+
+  /**
+   * Init a new game board with the specified size
+   * @todo: Finalize, see useContext?
+   */
+  const initNewGame = () => {
+    toggleNewGameModal()
+  }
+
   return (
     <nav className="navbar">
       <ul className="navbar-nav">
@@ -14,7 +32,7 @@ function NavBar() {
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">
+          <a className="nav-link" onClick={toggleNewGameModal}>
             <FontAwesomeIcon icon={faBorderAll} className="link-icon" />
             <span className="link-text">New game</span>
           </a>
@@ -26,6 +44,28 @@ function NavBar() {
           </a>
         </li>
       </ul>
+      {showNewGameModal ? (
+        <Modal>
+          <label htmlFor="select-new-game-size">
+            <h3>How big will the new game board be?</h3>
+            <select id="select-new-game-size">
+              {[3, 4, 5, 6].map((size, index) => (
+                <option value={size} key={index}>
+                  {size}
+                </option>
+              ))}
+            </select>
+          </label>
+          <div className="modal-button-container">
+            <button className="modal-button button-accept" onClick={initNewGame}>
+              New game
+            </button>
+            <button className="modal-button button-decline" onClick={toggleNewGameModal}>
+              Close
+            </button>
+          </div>
+        </Modal>
+      ) : null}
     </nav>
   )
 }
